fe41bbae636dd7f412a0cd6cdabc340526228b61
nathankairo <nathankairo@grossmaninteractive.com>
2014-11-14 20:10:07 +0530

    widget

--- application/businessadmin/views/widget.php
+++ application/businessadmin/views/widget.php
@@ -55,8 +55,9 @@
      
      <tr>
         <td>Widget</td>
-        <td class="widgetview"><a href="<?php echo $url[0]['siteurl'];?>widget/business/<?php echo $companyid;?>" target="_blank" title="Click to view your widget">Click to view your widget</a></td>
-        <td class="widgetcode">Please copy and paste this embed code into your HTML/website code to add this widget to your website.</td>
+        <td>
+		<a class="widgetview" href="<?php echo $url[0]['siteurl'];?>widget/business/<?php echo $companyid;?>" target="_blank" title="Click to view your widget">Click to view your widget</a>
+        Please copy and paste this embed code into your HTML/website code to add this widget to your website.</td>
       </tr>
       <tr>
         <td>Widget Code</td>
