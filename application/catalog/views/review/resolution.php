02171ebcea000fc9ee85354b0ae7d090dcc3aa4e
nathankairo <nathankairo@grossmaninteractive.com>
2014-12-04 20:34:35 +0530

    email

--- application/catalog/views/review/resolution.php
+++ application/catalog/views/review/resolution.php
@@ -4,30 +4,26 @@
     <div class="main_contentarea">
      <?php if($this->uri->segment(1) == 'review' && $this->uri->segment(2) == 'resolution') { ?>
 		  
-		  <div class="main_bd_srchwrp">
-			  <div class="bdsrch_wrp">
+		  <div class="main_bd_srchwrp reviewmail">
+			  <div class="bdsrch_wrp reviewmail_text">
 				<h2>Carrier</h2>
-				<div class="bd_srchwrp">
-				  <input type="text" required="" maxlength="100" name="carrier" class="bdsrch_txtbx">
+				<div class="bd_srchwrp reviewmail_outline">
+				  <input type="text" required="" maxlength="100" name="carrier" class="bdsrch_txtbx reviewmail_textbox">
 				</div>
 			  </div>
-			  <div class="bdsrch_wrp">
+			  <div class="bdsrch_wrp reviewmail_text">
 				<h2>Tracking Number</h2>
-				<div class="bd_srchwrp">
-				  <input type="text" required="" maxlength="100" name="trackingno" class="bdsrch_txtbx">
+				<div class="bd_srchwrp reviewmail_outline">
+				  <input type="text" required="" maxlength="100" name="trackingno" class="bdsrch_txtbx reviewmail_textbox">
 				</div>
 			  </div>
-			  <div class="bdsrch_wrp">
+			  <div class="bdsrch_wrp reviewmail_text">
 				<h2>Date Shipped</h2>
-				<div class="bd_srchwrp">
-				  <input type="text" required="" maxlength="100" name="dateshipped" class="bdsrch_txtbx">
+				<div class="bd_srchwrp reviewmail_outline">
+				  <input type="text" required="" maxlength="100" name="dateshipped" class="bdsrch_txtbx reviewmail_textbox">
 				</div>
-			  </div>
-			  <div class="bdsrch_wrp">
-				<div class="bd_srchwrp">
-				  <input type="submit" value="" name="submit" value = "submit">
-				</div>
-			  </div>
+			  </div>  
+			  <input type="submit" value="" name="submit" value = "submit">
         </div>
 	<?php } ?>
 	</div>
