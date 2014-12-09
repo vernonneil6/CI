b7c5d98f8c85a13a6bacf8adfc61cb0f7c1e6596
nathankairo <nathankairo@grossmaninteractive.com>
2014-12-10 00:40:43 +0530

    Removal tool

--- application/businessadmin/controllers/review.php
+++ application/businessadmin/controllers/review.php
@@ -270,6 +270,7 @@ class Review extends CI_Controller {
 		if( !$this->session->userdata('youg_admin'))
 	  	{
 		   	redirect('adminlogin', 'refresh');
+		   	$this->session->set_userdata('last_url','review/resolution/'.$reviewid);
 		}
 		
 		$companyid = $this->session->userdata['youg_admin']['id'];
