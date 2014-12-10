ff432369d7edb9c8f9c70b97ed5d3af06c941b6c
nathankairo <nathankairo@grossmaninteractive.com>
2014-12-11 00:31:59 +0530

    #19477

--- application/businessadmin/models/login.php
+++ application/businessadmin/models/login.php
@@ -54,6 +54,32 @@ Class Login extends CI_Model
 			return false;
 		}
  	}
+ 	function disablelogincheck($username,$password)
+ 	{
+   	   	$query = $this->db->get_where('company',  array( 'email' => $username,'password'=>$password));
+	    
+		
+		if ($query->num_rows() > 0)
+		{	
+			$company = $query->result_array();
+			$id = $company[0]['id'];
+			$query1 = $this->db->get_where('elite',  array( 'company_id' => $id,'status'=>'Disable'));
+			
+			if ($query1->num_rows() > 0)
+			{
+				$result = array( 'id' => $id,'username' => $username,'companyname'=>$company[0]['company']);
+				return $result;
+			}
+			else
+			{
+				return false;	
+			}
+		}
+		else
+		{
+			return false;
+		}
+ 	}
 	
 	
 	
