16b12c8afca873bee0681e565007e4430846f2e2
alankenn <alankenn@grossmaninteractive.com>
2015-01-19 21:10:05 +0530

    buyer badge #20068

--- application/businessadmin/controllers/buyer.php
+++ application/businessadmin/controllers/buyer.php
@@ -25,8 +25,8 @@ class buyer extends CI_Controller {
 		$this->data['site_name'] = $this->settings->get_setting_value(1);
 		$this->data['site_url'] = $this->settings->get_setting_value(2);
 		
-		$this->data['title'] = $this->settings->get_setting_value(1).' : Buyer Protection';
-		$this->data['section_title'] = 'Buyer Protection';
+		$this->data['title'] = $this->settings->get_setting_value(1).' : Buyer&#39;s Protection program';
+		$this->data['section_title'] = 'Buyer&#39;s Protection program';
 		$websites = $this->settings->get_all_urls();
 		
 		if( count($websites) > 0 )
