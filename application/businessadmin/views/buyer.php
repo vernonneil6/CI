14cfde571e34563f0c6ba2d9dafb85119193d1dc
alankenn <alankenn@grossmaninteractive.com>
2015-01-19 21:09:04 +0530

    buyer badge #20068

--- application/businessadmin/views/buyer.php
+++ application/businessadmin/views/buyer.php
@@ -6,22 +6,24 @@
   <div class="breadcrumbs">
     <ul>
       <li class="home"><a href="<?php echo site_url('dashboard');?>" title="Dashboard">Dashboard</a></li>
-      <li><a href="<?php echo site_url('badge');?>" title="<?php echo $section_title; ?>"><?php echo $section_title; ?></a></li>
+      <li><a href="<?php echo site_url('buyer');?>" title="<?php echo $section_title; ?>"><?php echo $section_title; ?></a></li>
     </ul>
   </div>
 
 
 <div class="box">
     <div class="headlines">
-      <h2><span><?php echo "Buyer" ?></span></h2>
+      <h2><span><?php echo $section_title; ?></span></h2>
     </div>
     
     <table class="tab tab-drag">
      <tbody><tr class="top nodrop nodrag"> </tr>       
       
       <tr class="odd">
-        <td>Buyer Protection Badge Url</td>
+        <td>Embeddable Buyer Protection Code:</td>
+        
         <td>
+        <p>To embed this code into your email or website, simply copy this code into your website's or outgoing email's code.</p>
         <textarea cols='90' rows='10'><a href="<?php echo 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME'].'/complaint/dispute/'.$this->session->userdata['youg_admin']['id'];?>" target="_blank" title="Get Widget">
         <img src="<?php echo 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME'].'/images/BuyersProtection_Badge.png'; ?>" class="logo_btm" alt="Yougotrated" title="Yougotrated"  width='10%' ></a></textarea>
         </td>
@@ -33,24 +35,17 @@
         <td>
     <a href="<?php echo 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME'].'/complaint/dispute/'.$this->session->userdata['youg_admin']['id'];?>" target="_blank" title="Get Widget">
         <img src="<?php echo 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME'].'/images/BuyersProtection_Badge.png'; ?>" class="logo_btm" alt="Yougotrated" title="Yougotrated"  width='10%' ></a>
+        <br>
+        <span class="badge_font">
+			Your purchase is protected by the <br> YouGotRated Buyers Protection program.<br>
+            Transaction ID: <?php echo $this->session->userdata['youg_admin']['id'];?> <br> 
+            To file a complaint -<a href="<?php echo 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME'].'/complaint/dispute/'.$this->session->userdata['youg_admin']['id'];?>" target="_blank" title="File Complaint">Click Here</a>
+        </span>
         </td>
       </tr>
     
     </tbody>
     </table>	
-  
-  <table class="tab tab-drag">
-     <tbody><tr class="top nodrop nodrag"> </tr> 
-      <tr class="odd">
-        <td>
-        <p>Your purchase is protected by the YouGotRated Buyers Protection program.</p>
-        <p>Transaction ID: <?php echo $this->session->userdata['youg_admin']['id'];?> <br>
-        To file a complaint -<a href="<?php echo 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME'].'/complaint/dispute/'.$this->session->userdata['youg_admin']['id'];?>" target="_blank" title="File Complaint">Click Here</a><p>
-        </td>
-    </tr>
-
-  </tbody>
-    </table>      
    
 </div>
 <script>
