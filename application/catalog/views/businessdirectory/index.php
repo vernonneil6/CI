e24d9e144c33f77ca51f10d5824dd101b0089525
nathankairo <nathankairo@grossmaninteractive.com>
2015-01-20 17:13:43 +0530

    removing searchbar

--- application/catalog/views/businessdirectory/index.php
+++ application/catalog/views/businessdirectory/index.php
@@ -3,19 +3,23 @@
 <section class="container">
   <section class="main_contentarea">
     <div class="innr_wrap">
-      <div class="bus_dir_head">
+		<div class="bus_dir_head">
 			<h1 class="bannertextcoupon">
-   		<span class="bannertextregular">Business </span>Directory
+				<span class="bannertextregular">Business </span>Directory
      		</h1>
-	</div>
-	
-	<div class='headersearch'>
-	  <?php echo form_open('businessdirectory/search',array('class'=>'formBox','name'=>'frmsearch','id'=>'frmsearch')); ?>
-          <?php if( $this->uri->segment(1)=='complaint' && $this->uri->segment(2)=='search') { $serkeyword=base64_decode($this->uri->segment(3));} else { $serkeyword =''; } ?>
-		<input type='text'  class='headersearchbar' placeholder="Search for a Business..." name="searchcomp"  id="search" value="<?php echo $serkeyword;?>" required maxlength="30">
-		<input type="submit" class="headersearchbtn" value="SEARCH" name="btnsearch">
-	 <?php echo form_close();?> 
-    </div>
+		</div>
+		
+		<div class='headersearch'>
+		  <?php echo form_open('businessdirectory/search',array('class'=>'formBox','name'=>'frmsearch','id'=>'frmcompany')); ?>
+			  <?php if( $this->uri->segment(1)=='complaint' && $this->uri->segment(2)=='search') { $serkeyword=base64_decode($this->uri->segment(3));} else { $serkeyword =''; } ?>
+			<input type='text'  class='headersearchbar' placeholder="Search for a Business..." name="searchcomp"  id="search" value="<?php echo $serkeyword;?>" required maxlength="30">
+			<input type="submit" class="headersearchbtn" value="SEARCH" name="btnsearch">
+			<div class="orwrp">
+				<div class="orinnrwrp"> </div>
+				<h1 class="bus_tag"><a href="<?php echo site_url('businessdirectory/add');?>" title="Submit Business To YouGotRated Directory" style="color:#0080FF;">SUBMIT A NEW BUSINESS</a></h1>
+			</div>
+			<div class="lgn_btnlogo"> <a href="<?php echo base_url();?>"><img src="images/ygr_logos.png" class="logo_btm" alt="Yougotrated" title="Yougotrated"></a> </div>
+		
      <?php /* <form class="busdt_wrap" method="post" id="frmcompany" action="businessdirectory/search">
         <div class="main_bd_srchwrp">
           <div class="bdsrch_wrp">
@@ -32,6 +36,8 @@
         </div>
         <div class="lgn_btnlogo"> <a href="<?php echo base_url();?>"><img src="images/ygr_logos.png" class="logo_btm" alt="Yougotrated" title="Yougotrated"></a> </div>
       </form>*/ ?>
+		 <?php echo form_close();?> 
+		</div>
     </div>
   </section>
 </section>
