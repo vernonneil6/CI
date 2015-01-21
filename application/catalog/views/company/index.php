7e6b9744615de8f9daeaa228276f928bf253ef1f
nathankairo <nathankairo@grossmaninteractive.com>
2015-01-22 00:27:25 +0530

    css

--- application/catalog/views/company/index.php
+++ application/catalog/views/company/index.php
@@ -569,7 +569,7 @@ Reviews
 			    if(count($users)>0) 
 			    { 
 				?>
-				<div class ="review_border_bottom">
+				<div class ="<?php if($i!='1'){ echo "review_border_bottom";} ?> padding_top_1">
 				 <div class = "review_firstletter"><label><?php $firstword = $users['username']; echo $firstword[0];?></label></div>
                  <div class = "review_username_row">
 					 <div class = "review_name_tab tooltip" title = "This review has been authenticated by <?php echo $cmpy['company']; ?> and has been posted on YouGotRated by a real shopper">
