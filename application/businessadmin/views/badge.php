2d972d1a4f1296cc4178457aad57d17a8bce9b83
nathankairo <nathankairo@grossmaninteractive.com>
2015-01-21 17:44:04 +0530

    badge removed review

--- application/businessadmin/views/badge.php
+++ application/businessadmin/views/badge.php
@@ -1,11 +1,3 @@
-<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/tooltipster.css" />
-<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.tooltipster.min.js"></script>
-<script>
-	$(document).ready(function() {
-		$('.tooltip').tooltipster();
-	});
-</script>
-
 <?php echo $header; ?>
 
 
@@ -29,13 +21,13 @@
 		 <tr class="odd">
 			<td>Badge Url</td>
 			<td>
-			<textarea cols='90' rows='10'><a href="<?php echo 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME'].'/badge/rating/'.md5($this->session->userdata['youg_admin']['id']);?>" target="_blank" title="Get Widget">
-			<img src="<?php echo 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME'].'/images/badge.png'; ?>" class="logo_btm" alt="Yougotrated" title="Yougotrated"  width='7%' ></a></textarea></td></tr>
+			<textarea cols='90' rows='10'><a href="#">
+			<img src="<?php echo 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME'].'/images/badge.png'; ?>" class="logo_btm tooltip" alt="Yougotrated" title="The company is a verified merchant."   width='7%' ></a></textarea></td></tr>
 			<tr class="odd">
 			<td>Sample Image</td>
 			<td>
-			<a href="#" class = "tooltip" target="_blank" title="Get Widget">
-			<img src="<?php echo 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME'].'/images/badge.png'; ?>" class="logo_btm" alt="Yougotrated" title="Yougotrated"  width='10%' ></a>
+			<a href="#">
+			<img src="<?php echo 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME'].'/images/badge.png'; ?>" class="logo_btm tooltip" alt="Yougotrated" title="The company is a verified merchant."  width='10%' ></a>
 			</td>
 		  </tr>
 		</tbody>
@@ -45,8 +37,17 @@
 </div>
 
 <div class="tooltip"> 
-   The company is a verified merchant.
+   
 </div>
 
 <?php include('leftmenu.php'); ?>
 <?php echo $footer; ?>
+
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/tooltipster.css" />
+<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.tooltipster.min.js"></script>
+<script>
+	$(document).ready(function() {
+		$('.tooltip').tooltipster();
+	});
+</script>
+
