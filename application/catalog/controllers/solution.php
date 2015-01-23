c056acf058847ab2d21c13936682302aaf4f2f05
alankenn <alankenn@grossmaninteractive.com>
2015-01-23 19:52:02 +0530

    solution checked

--- application/catalog/controllers/solution.php
+++ application/catalog/controllers/solution.php
@@ -1691,10 +1691,10 @@ public function upgrades($id)
 	   $host = "apitest.authorize.net";*/
 	
 	
-	/*live
+	/*live*/
 	    $loginname="5h7G7Sbr";
 		$transactionkey="94KU7Sznk72Kj3HK";
-		$host = "api.authorize.net";*/
+		$host = "api.authorize.net";
 	
 	
 	$path = "/xml/v1/request.api";
