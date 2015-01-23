b281c44e87e6d54687fc6d26c136a98ca8316f14
nathankairo <nathankairo@grossmaninteractive.com>
2015-01-23 21:53:53 +0530

    description

--- application/catalog/controllers/company.php
+++ application/catalog/controllers/company.php
@@ -28,12 +28,16 @@ class Company extends CI_Controller {
 		$url = 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
   		$pieces = parse_url($url);
 		$domain = isset($pieces['host']) ? $pieces['host'] : '';
-		if (preg_match('/(?P<domain>[a-z0-9][a-z0-9\-]{1,63}\.[a-z\.]{2,6})$/i', $domain, $regs))
-		 {
+		if (preg_match("/\writerbin\b/i", $domain, $regs)) 
+		{
+			$site = 'yougotrated.writerbin.com';
+		}
+		else if(preg_match('/(?P<domain>[a-z0-9][a-z0-9\-]{1,63}\.[a-z\.]{2,6})$/i', $domain, $regs))
+		{
 		    $site = $regs['domain'];
-		 }
+		}
 		 
-		 $website = $this->common->get_site_by_domain_name('yougotrated.writerbin.com');
+		 $website = $this->common->get_site_by_domain_name($site);
 		 
 		 if(count($website)>0)
 		 {
