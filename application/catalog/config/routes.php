cbe4720f64bc1023132adeaa0bef373c5df2c0c0
nathankairo <nathankairo@grossmaninteractive.com>
2015-01-26 17:31:40 +0530

    url

--- application/catalog/config/routes.php
+++ application/catalog/config/routes.php
@@ -41,6 +41,7 @@
 $route['default_controller'] = "welcome";
 $route['404_override'] = 'error';
 $route['company/([a-z0-9-]+)/([a-z0-9-]+)/([a-z0-9-]+)/([a-z0-9-]+)'] = "company/index/$1/$2/$3/$4";
+$route['xhr/(:any)'] = '$1';  
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
