e6211a72e7d0a081d7a04c39acf7c40e67635f1f
alankenn <alankenn@grossmaninteractive.com>
2015-01-26 18:47:32 +0530

    #20192 -Authorize changes check

--- application/catalog/controllers/solution.php
+++ application/catalog/controllers/solution.php
@@ -21,7 +21,7 @@ class Solution extends CI_Controller {
 				$site = $regs['domain'];
 			}
 		}
-		 $website = $this->common->get_site_by_domain_name('yougotrated.writerbin.com');
+		 $website = $this->common->get_site_by_domain_name($site);
 		 
 		 if(count($website)>0)
 		 {
