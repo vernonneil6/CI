63f0f4081b920b0844f035b055a0519133351313
nathankairo <nathankairo@grossmaninteractive.com>
2015-01-28 17:44:38 +0530

    video

--- application/businessadmin/controllers/video.php
+++ application/businessadmin/controllers/video.php
@@ -223,15 +223,15 @@ class Video extends CI_Controller {
 		
 		if(preg_match("/youtu.be\/[a-z1-9.-_]+/", $this->input->post('addvideourl'))) 
 		{
-			$videourl = preg_replace('~^https?://youtu\.be/([a-z\d]+)$~i', 'www.youtube.com/watch?v=$1', $this->input->post('addvideourl'));
+			$videourl = preg_replace('~^https?://youtu\.be/([a-z\d]+)$~i', 'http://www.youtube.com/watch?v=$1', $this->input->post('addvideourl'));
 		}
 		else if(preg_match("/y2u.be\/[a-z1-9.-_]+/", $this->input->post('addvideourl'))) 
 		{
-			$videourl = preg_replace('~^https?://y2u\.be/([a-z\d]+)$~i', 'www.youtube.com/watch?v=$1', $this->input->post('addvideourl'));
+			$videourl = preg_replace('~^https?://y2u\.be/([a-z\d]+)$~i', 'http://www.youtube.com/watch?v=$1', $this->input->post('addvideourl'));
 		}
 		else
 		{
-			$videourl = preg_replace('#^https?://#', '', $this->input->post('addvideourl'));
+			$videourl = preg_replace("/^https:/i", "http:", $this->input->post('addvideourl'));
 		}
 		
 
