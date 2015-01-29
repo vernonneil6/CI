dd1c8a23cac1c14c1c4c8f4af6a1ccf9c4116aae
nathankairo <nathankairo@grossmaninteractive.com>
2015-01-29 17:23:08 +0530

    editor

--- application/catalog/views/footerpage.php
+++ application/catalog/views/footerpage.php
@@ -4,7 +4,7 @@
   <section class="main_contentarea">
     <div class="innr_wrap">
       <h1 class="bannertext font_size_banner"><?php echo $varheading; ?></h1>
-      <div class="footer_page_content"><?php echo stripslashes($content); ?></div>    
+      <div class="footer_page_content"><?php echo preg_replace('/<br[^>]*>/i', '', stripslashes($content)); ?></div>
     </div>
   </section>
 </section>
