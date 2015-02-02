7742ab38358720da8f2ba491e5dbb69bf8c0873a
nathankairo <nathankairo@grossmaninteractive.com>
2015-02-03 01:37:12 +0530

    timer

--- application/catalog/views/company/index.php
+++ application/catalog/views/company/index.php
@@ -346,21 +346,19 @@
 			    $users = $this->users->get_user_bysingleid($reviews[$i]['reviewby']); 
 			    if(count($users)>0) 
 			    { 
-					 if($user['username']!='')
+					 if($user[0]['username']!='')
 					  {
 					
 			?>
                   <a><span itemprop = "author"><?php echo $users['username'];?></span></a>
 			<?php
-				}
-			
-			else
+				}else
 					{
 					?>
 						<a><?php echo "Anonymous"; ?></a>
 					<?php
 					}
-				
+			}
 			?>
             <div class="datereview"><span itemprop = "datePublished"><?php echo date("m/d/Y",strtotime($reviews[$i]['reviewdate']));?></span></div>
                   <?php  } else {?>       
