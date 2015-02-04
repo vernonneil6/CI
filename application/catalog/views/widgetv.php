d3a6230c6c733d916f6d4a6fa19040bbc43e6a62
nathankairo <nathankairo@grossmaninteractive.com>
2015-02-05 00:30:23 +0530

    widget

--- application/catalog/views/widgetv.php
+++ application/catalog/views/widgetv.php
@@ -35,9 +35,10 @@ Reviews
 					<img src="<?php echo 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME'].'/'; ?>images/no_star.png" alt="no_star" title="no_star" />
 					<?php } ?>
 					<label class = "single_review"><?php echo $total."  Reviews"; ?></label>
-				  </div>
-				  
-			  </div>
+				  </div>				  
+			</div>
+			  
+			  
             <?php 
             if( count($reviews) > 0 ) 
             { 		
@@ -99,6 +100,75 @@ Reviews
 								   <p><?php echo $reviews[$i]['comment'];?></p>		  
 							 </div>
 							 
+							 <div class = "review_rates">
+								 <div class = "review_ratethis">
+									<span>
+									  <label>RATE THIS REVIEW:</label>
+									  <?php $ip = $_SERVER['REMOTE_ADDR'];?>
+									  <?php if($this->reviews->check_vote($ip,$reviews[$i]['id'],'helpful') == 'true'){ ?>
+									  <a class="vote-disable" id="helpful_<?php echo $reviews[$i]['id'];?>" reviewid="<?php echo $reviews[$i]['id'];?>" title="Helpful" style="cursor:pointer !important;"><b id="helpful<?php echo $reviews[$i]['id'];?>"><?php echo $this->reviews->getcount($reviews[$i]['id'],'helpful');?></b> Helpful</a>
+									  <?php }else{ ?>
+									  <a class="vote" id="helpful_<?php echo $reviews[$i]['id'];?>" reviewid="<?php echo $reviews[$i]['id'];?>" title="Helpful" style="cursor:pointer !important;">Helpful</a>
+									  <?php } ?>
+									  <?php if($this->reviews->check_vote($ip,$reviews[$i]['id'],'funny') == 'true'){?>
+									  <a id="funny_<?php echo $reviews[$i]['id'];?>" class="vote-disable" title="funny" reviewid="<?php echo $reviews[$i]['id'];?>" style="cursor:pointer !important;"><b id="funny<?php echo $reviews[$i]['id'];?>"><?php echo $this->reviews->getcount($reviews[$i]['id'],'funny');?></b> Funny</a>
+									  <?php }else{ ?>
+									  <a id="funny_<?php echo $reviews[$i]['id'];?>" class="vote" title="funny" reviewid="<?php echo $reviews[$i]['id'];?>" style="cursor:pointer !important;">Funny</a>
+									  <?php } ?>
+									  <?php if($this->reviews->check_vote($ip,$reviews[$i]['id'],'agree') == 'true'){?>
+									  <a id="agree_<?php echo $reviews[$i]['id'];?>" class="vote-disable" reviewid="<?php echo $reviews[$i]['id'];?>" title="Agree" style="cursor:pointer !important;"><b id="agree<?php echo $reviews[$i]['id'];?>"><?php echo $this->reviews->getcount($reviews[$i]['id'],'agree');?></b> Agree</a>
+									  <?php }else{ ?>
+									  <a id="agree_<?php echo $reviews[$i]['id'];?>" class="vote" reviewid="<?php echo $reviews[$i]['id'];?>" title="Agree" style="cursor:pointer !important;">Agree</a>
+									  <?php } ?>
+									  <?php if($this->reviews->check_vote($ip,$reviews[$i]['id'],'disagree') == 'true'){?>
+									  <a id="disagree_<?php echo $reviews[$i]['id'];?>" class="vote-disable" title="disagree" reviewid="<?php echo $reviews[$i]['id'];?>" style="cursor:pointer !important;"><b id="disagree<?php echo $reviews[$i]['id'];?>"><?php echo $this->reviews->getcount($reviews[$i]['id'],'disagree');?></b> Disagree</a>
+									  <?php }else{ ?>
+									  <a id="disagree_<?php echo $reviews[$i]['id'];?>" class="vote" title="disagree" reviewid="<?php echo $reviews[$i]['id'];?>" style="cursor:pointer !important;">Disagree</a>
+									  <?php } ?>
+									  <?php if($this->session->userdata('youg_user')){$userid = $this->session->userdata['youg_user']['userid']; ?>
+									  <?php if($reviews[$i]['reviewby'] == $userid) {?>
+									  <a href="<?php echo site_url('review/edit').'/'.$reviews[$i]['id'];?>" title="Edit Review" class="" style="cursor:pointer !important;">Edit</a> <a href="<?php echo site_url('review/delete').'/'.$reviews[$i]['id'];?>" title="Delete Review" class="" onclick="return confirm('Are you sure to remove this review?');" style="cursor:pointer !important;">Delete</a>
+									  <?php } else {?>
+									  <a href="<?php echo site_url('review/add').'/'.$reviews[$i]['companyid'];?>" title="Review This Company" class="dir-searchbtn" style="cursor:pointer !important;">Review It</a>
+									  <?php } ?>
+									  <?php } else {?>
+									  <a href="<?php echo site_url('review/add').'/'.$reviews[$i]['companyid'];?>" title="Review This Company" class="dir-searchbtn" style="cursor:pointer !important;">Review It</a>
+									  <?php } ?>   
+										  <script>
+											<?php $ip = $_SERVER['REMOTE_ADDR'];  ?>
+											
+											$(function(){ 
+											 $("#helpful_<?php echo $reviews[$i]['id'];?>").click(function() {
+												 var vote = 'helpful';
+												 var reviewid = $(this).attr('reviewid');
+												 check('<?php echo $ip;?>',reviewid,vote);
+											   });
+											   $("#funny_<?php echo $reviews[$i]['id'];?>").click(function() {
+												 var vote = 'funny';
+												 var reviewid = $(this).attr('reviewid');
+												 check('<?php echo $ip;?>',reviewid,vote);
+											   });
+											   $("#agree_<?php echo $reviews[$i]['id'];?>").click(function() {
+												 var vote = 'agree';
+												 var reviewid = $(this).attr('reviewid');
+												 check('<?php echo $ip;?>',reviewid,vote);
+												 countme(reviewid,'disagree');
+												 $('#disagree_'+reviewid).removeClass('vote-disable');
+												 $('#disagree_'+reviewid).addClass('vote'); 
+											   });
+											   $("#disagree_<?php echo $reviews[$i]['id'];?>").click(function() {
+												 var vote = 'disagree';
+												 var reviewid = $(this).attr('reviewid');
+												 check('<?php echo $ip;?>',reviewid,vote);
+												 countme(reviewid,'agree');
+												 $('#agree_'+reviewid).removeClass('vote-disable');
+												 $('#agree_'+reviewid).addClass('vote'); 
+											   });
+											});
+										</script>           
+									</span>
+								</div>
+							 </div>
 							 
 						 </div> 
 						<?php
@@ -133,4 +203,51 @@ Reviews
 		
 		$('.fancybox').fancybox();
 	});
+
+
+function countme(rid,vote)
+	{
+	  $.ajax({
+		  type 				: "POST",
+		  url 				: "<?php echo site_url('review/countme');?>",
+		  dataType 			: "json",
+		  data				: {reviewid:rid,vote : vote},
+		  cache				: false,
+		  success			: function(data)
+							  {	
+								$('#'+vote+'_'+rid).html("<b>"+data.total+"</b>&nbsp;"+vote);
+							
+							  }
+	   });
+	}
+	
+	function check(ip,rid,vote)
+	{
+	  
+	  $.ajax({
+		  type 				: "POST",
+		  url 				: "<?php echo site_url('review/checkvote');?>",
+		  dataType 			: "json",
+		  data				: { ip:ip,reviewid:rid,vote : vote},
+		  cache				: false,
+		  success			: function(data)
+							  {	
+								if(data.message == 'deleted')
+								{
+								   $('#'+vote+'_'+rid).removeClass('vote-disable');
+								   $('#'+vote+'_'+rid).addClass('vote');
+								}
+								if(data.message == 'added')
+								{
+								   $('#'+vote+'_'+rid).removeClass('vote');
+								   $('#'+vote+'_'+rid).addClass('vote-disable');										   										  
+								}
+								countme(rid,vote);
+							  }
+		  
+
+	   });
+	  
+	}
+
 </script>
