1e2839551373eb4585154b5f48a82df33806babe
alankenn <alankenn@grossmaninteractive.com>
2015-02-06 17:36:19 +0530

    #20311-check

--- application/catalog/views/solution/claimbusiness.php
+++ application/catalog/views/solution/claimbusiness.php
@@ -224,13 +224,14 @@
 <script>
 function chkwebsite(website){
 	$("#websitevaliderror").hide();
-	var filter  = /^(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	var filter  = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/;
 	if( trim(website) != '' && filter.test(trim(website)) )
 	{
 		return true;
 	}else{
 		$("#websiteerror").hide();
 		$("#websitevaliderror").show();
+		$("#websitevaliderror").text('Please enter a valid URL. For example http://www.example.com or https://www.example.com');
 		$("#website").focus();
 		return false;
 	}
