27dfc6ae718dbe143ef67f1048433ac211684589
alankenn <alankenn@grossmaninteractive.com>
2015-02-06 17:55:36 +0530

    js validation

--- application/catalog/views/solution/claimbusiness.php
+++ application/catalog/views/solution/claimbusiness.php
@@ -42,6 +42,7 @@
             <input type="text" class="reg_txt_box" placeholder="WEBSITE" id="website" name="website"  maxlength="150" value="<?php echo $showdata['siteurl']; ?>" onchange="chkwebsite(this.value);">            
             <div id="websiteerror" class="error">Website is required.</div>
             <div id="websitevaliderror" class="error">Enter valid Website.</div> 
+            <div id="websitevalidsuccess" class="cardsuccess" style="display:none">Its a valid website.</div> 
           </div>
           <div class="reg-row" style="margin-top:10px;">
             <div class="reg_fld">CATEGORY</div>
@@ -227,6 +228,7 @@ function chkwebsite(website){
 	var filter  = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/;
 	if( trim(website) != '' && filter.test(trim(website)) )
 	{
+		$('#websitevalidsuccess').show(); 
 		return true;
 	}else{
 		$("#websiteerror").hide();
