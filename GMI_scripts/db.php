c57fbb6c13439dc3c4332f34c8b6b88e9f9ad5ab
nathankairo <nathankairo@grossmaninteractive.com>
2015-02-12 12:01:26 -0500

    sitemap

--- GMI_scripts/db.php
+++ GMI_scripts/db.php
@@ -1,14 +1,31 @@
 <?php 
-$servername = "localhost";
-$username = "mxiind_hitesh";
-$password = "vS^T+ymX)~)P";
-$dbname = "mxiind_yougotrated";
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if($_SERVER['SERVER_NAME'] == 'www.yougotrated.writerbin.com'){
+	$servername = "localhost";
+	$username = "mxiind_hitesh";
+	$password = "vS^T+ymX)~)P";
+	$dbname = "mxiind_yougotrated";
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	}
+	$row_count = 49000;
+	$no_of_rows = 0;
+}elseif($_SERVER['SERVER_NAME'] == 'www.yougotrated.writerbin.com'){
+	$servername = "localhost";
+	$username = "root";
+	$password = "ygrmysql";
+	$dbname = "mxiind_yougotrated";
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	}
+	$row_count = 49000;
+	$no_of_rows = 0;
+}else{
+	echo "DB Connection Failed"
 }
-$row_count = 25000;
-$no_of_rows = 0;
 ?>
