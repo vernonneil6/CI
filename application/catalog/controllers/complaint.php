599d35e285113829520808f90fb396126f975f88
alankenn <alankenn@grossmaninteractive.com>
2015-02-17 19:08:50 +0530

    complaint

--- application/catalog/controllers/complaint.php
+++ application/catalog/controllers/complaint.php
@@ -38,7 +38,7 @@ class Complaint extends CI_Controller {
 		    $site = $regs['domain'];
 		}
 		 
-		 $website = $this->common->get_site_by_domain_name('yougotrated.writerbin.com');
+		 $website = $this->common->get_site_by_domain_name($site);
 		 
 		 if(count($website)>0)
 		 {
