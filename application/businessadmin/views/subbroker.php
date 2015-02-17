3f20f97267b375019c0747abd8fe1b552271191b
nathankairo <nathankairo@grossmaninteractive.com>
2015-02-17 20:58:48 +0530

    redirection

--- application/businessadmin/views/subbroker.php
+++ application/businessadmin/views/subbroker.php
@@ -21,7 +21,10 @@
         <td>Sub Broker Url</td>
         <td>
         <textarea cols='90' rows='10'>
-			<?php echo 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME'].'/signuppage/affid/'.$this->session->userdata['subbroker_data'][0]->id;?>
+			<?php 
+				$url = $this->setting->get_url_by_id(17); 
+				echo $url[0]['siteurl'].'?affid='.$this->session->userdata['subbroker_data'][0]->id;
+			?>
         </textarea>
         </td>
 	 </tr>
