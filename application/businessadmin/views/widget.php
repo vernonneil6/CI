9b75efea8df5470bd76ff1876131f982c467c469
Vernon Neil <vernonneil@grossmaninteractive.com>
2015-02-20 19:14:34 +0000

    widget popup

--- application/businessadmin/views/widget.php
+++ application/businessadmin/views/widget.php
@@ -70,7 +70,7 @@
 	<div class="company_review_tab fancybox" href="#review_popup" onclick="showPopup()">&nbsp;</div>
 	<div id="review_popup" class = "popupwidth">
 		<div class='close_popup' onclick="closePopup()">&nbsp</div>		
-		<iframe id="container_frame" src="http://www.yougotrated.com/widget/content/<?php echo $companyid; ?>" style="width:100%;height:100%;position:relative;" ></iframe></div> 		
+		<iframe id="container_frame" src="<?php echo 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME']. '/widget/content/'.$companyid; ?>" style="width:100%;height:100%;position:relative;border:0 none;" ></iframe></div> 		
 	</div>
 	
 
