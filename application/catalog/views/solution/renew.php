08b9e6a1d77cffbb05c236889ba44b8ee9eafa76
Vernon Neil <vernonneil@grossmaninteractive.com>
2015-02-26 07:28:03 -0600

    renew

--- application/catalog/views/solution/renew.php
+++ application/catalog/views/solution/renew.php
@@ -1,5 +1,5 @@
 <?php echo $header;?>
-<script type="text/javascript" src="js/formsubmit.js"></script>
+
 
 <section class="container">
   <section class="main_contentarea">
@@ -8,7 +8,7 @@
       <div class="reg_step_edit_claim" style="display:none;"></div>
       <div class="reg_frm_wrap">
 	      
-        <form class="reg_frm" action="index.php/solution/renew_update/<?php echo $renewelite[0]['id'];?>" id="frmaddcompany" method="post" enctype="multipart/form-data">
+        <form class="reg_frm" action="index.php/solution/receipt" id="frmaddcompany" method="post" enctype="multipart/form-data">
           <div class="reg-row">
             <label>RENEW YOUR SUBSCRIPTION HERE</label>
             <div class="reg_fld">PLEASE RENEW YOUR ELITEMEMBERSHIP SUBSCRIPTION HERE FOR ACTIVATING IT.</div>
@@ -16,7 +16,15 @@
             <input type="hidden" class="reg_txt_box" placeholder="companyid" id="companyid" name="companyid"  maxlength="30" value="<?php echo $renewelite[0]['id'];?>" />
             <input type="text" class="reg_txt_box" placeholder="NAME" id="name" name="name"  maxlength="30" value="<?php echo $renewelite[0]['company'];?>" />
             <input type="hidden" class="reg_txt_box" placeholder="WEBSITE" id="website" name="website"  maxlength="150" value="<?php echo $renewelite[0]['siteurl'];?>" />
+            <input type="hidden" class="reg_txt_box-lg" placeholder="category" name="cat" id="category" maxlength="50" value="<?php echo $renewelite[0]['categoryid'];?>" />
+            <input type="hidden" class="reg_txt_box-lg" placeholder="category" name="categorylist" id="category" maxlength="50" value="" />
             <input type="hidden" class="reg_txt_box" placeholder="E-MAIL ADDRESS" id="email" name="email"  maxlength="250" value="<?php echo $renewelite[0]['email'];?>" />
+            <input type="hidden" class="reg_txt_box"  id="streetaddress1" name="streetaddress1"  maxlength="250" value="<?php echo $renewelite[0]['companystreet'];?>" />
+            <input type="hidden" class="reg_txt_box"  id="country1" name="countryname1"  maxlength="250" value="<?php echo $renewelite[0]['companycountry'];?>" />
+            <input type="hidden" class="reg_txt_box"  id="state1" name="state1"  maxlength="250" value="<?php echo $renewelite[0]['companystate'];?>" />
+            <input type="hidden" class="reg_txt_box"  id="city1" name="city1"  maxlength="250" value="<?php echo $renewelite[0]['companycity'];?>" />
+            <input type="hidden" class="reg_txt_box"  id="zip1" name="zip1"  maxlength="250" value="<?php echo $renewelite[0]['companyzip'];?>" />
+            <input type="hidden" class="reg_txt_box"  id="phone" name="phone"  maxlength="250" value="<?php echo $renewelite[0]['phone'];?>" />
             <input type="hidden" class="reg_txt_box-lg" placeholder="cname" name="cname" id="cname" maxlength="50" value="<?php echo $renewelite[0]['contactname'];?>" />
             <input type="hidden" class="reg_txt_box-lg" placeholder="cphone" name="cphone" id="cphone" maxlength="50" value="<?php echo $renewelite[0]['contactphonenumber'];?>" />
             <input type="hidden" class="reg_txt_box-lg" placeholder="cemail" name="cemail" id="cemail" maxlength="50" value="<?php echo $renewelite[0]['contactemail'];?>" />
@@ -48,6 +56,7 @@
 			</style>
           <div style="float:left;">
            <?php echo form_dropdown('country',$selcon,'','id="country" class="seldrop" onchange=getstates(this.value,"state","#selstatediv");');?>
+           <input type="hidden" name="countryname" id="countryname"/>
            </div>
            <?php 
 		  $selstate=array(''=>'--Select State--');
@@ -75,20 +84,42 @@
             <div id="carderror" class="carderror"></div>
             <div id="cardsuccess" class="cardsuccess"></div>   
           </div>
+          
+           <div class="reg-row" style="margin-top:10px !important;">
+				<div>
+					<span class="form-col-1">
+						&nbsp;
+					</span>
+					<span class="form-col-2">
+						<div class="reg_fld">CVV Code:</div>
+         
+              
+					  <input type="text" name="cvv" value="" id="cvv" class="input" placeholder="CVV Code">			       
+					  
+					   <a class="cvvpop" href="http://yougotrated.com/businessadmin/">What is this? 
+							<img src="images/cvvpop1.jpg" id="cvvhover" style="display: none;">
+					   </a>   
+					</span>
+				</div>
+			   
+                         
+        </div>
+          <div style="clear: both;"></div>
+          
           <div class="reg-row" style="margin-top:10px !important;">
-            <div class="reg_fld">EXPIRATION DATE</div>
-            <select id="expirationdatey" name="expirationdatey">
-                <option value="">--Select--</option>
-                <?php for($k=0;$k<10;$k++) {?>
-                <?php $a = date('Y')+$k;?>
-                <option value="<?php echo $a;?>"><?php echo $a;?></option>
-                <?php } ?>
-              </select>
-              &nbsp;
+            <div class="reg_fld">EXPIRATION DATE</div>          
               <select id="expirationdatem" name="expirationdatem">
                 <option value="">--Select--</option>
                 <?php for($i=1;$i<13;$i++) {?>
                 <option value="<?php echo $i;?>"><?php echo $i;?></option>
+                <?php } ?>
+              </select>
+              &nbsp;
+              <select id="expirationdatey" name="expirationdatey">
+                <option value="">--Select--</option>
+                <?php for($k=0;$k<10;$k++) {?>
+                <?php $a = date('Y')+$k;?>
+                <option value="<?php echo $a;?>"><?php echo $a;?></option>
                 <?php } ?>
               </select>
               <div id="ccnumbererror" class="error">Credit Card Number is required.</div><div id="expirationdateerror" class="error">Select Expiration Date.</div>
