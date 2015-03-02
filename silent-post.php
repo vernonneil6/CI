c9a08690f17b23f55469ccfc723aac331ec012f4
alankenn <alankenn@grossmaninteractive.com>
2015-03-02 18:54:13 +0530

    slient

--- silent-post.php
+++ silent-post.php
@@ -43,7 +43,9 @@ if($sql!='')
 {
  if($tn_status==1){
 	 $tn_status=1;
-	 $query="UPDATE youg_subscription SET `datereturn`='$date' , `transactionstatus`='$tn_status' , `transactionresponse`='$transaction_status' WHERE subscr_id='$sub_id'";
+	 $expires = date('Y-m-d H:i:s', strtotime("+$time Month"));
+	 $paymentdate =date("Y-m-d H:i:s");
+	 $query="UPDATE youg_subscription SET `payment_date`='$paymentdate',`expires`='$expires',`datereturn`='$date' , `transactionstatus`='$tn_status' , `transactionresponse`='$transaction_status' WHERE subscr_id='$sub_id'";
  } else {
 	 $tn_status=0;
 	 $query="UPDATE youg_subscription SET `datereturn`='$date' , `transactionstatus`='$tn_status' ,`transactionresponse`='$transaction_status' WHERE subscr_id='$sub_id'";	  
