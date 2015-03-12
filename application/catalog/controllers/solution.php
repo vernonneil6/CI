942c3f274351f97821b2f29bbfd83356ba82716f
nathankairo <nathankairo@grossmaninteractive.com>
2015-03-12 13:10:01 -0400

    affid proble

--- application/catalog/controllers/solution.php
+++ application/catalog/controllers/solution.php
@@ -177,8 +177,10 @@ class Solution extends CI_Controller {
 	}
 	function claimbusiness()
 	{   
+		$upgradefn=$this->uri->segment(3);
 		$upgradeid=$this->uri->segment(4); 
- 		if(isset($upgradeid)){
+		
+ 		if(!empty($upgradeid) && ($upgradefn=='elitemem')){
 		    $eid=$upgradeid;
 		}else{
 		    $eid="";
@@ -682,16 +684,16 @@ public function eliteSubscribe($formpost,$companyid) {
 	   $transactionkey="38UzuaL2c6y5BQ88";
 	   $host = "apitest.authorize.net"; */
 	
-	/*sandbox test mode*/
+	/*sandbox test mode
 	
 	  $loginname="83EK7S4R8qy3";
 	   $transactionkey="5Rx8Mn8PAS5s77gr";
-	   $host = "apitest.authorize.net";
+	   $host = "apitest.authorize.net";*/
 	
-	/*live
+	/*live*/
 	    $loginname="5h7G7Sbr";
 		$transactionkey="94KU7Sznk72Kj3HK";
-		$host = "api.authorize.net";*/
+		$host = "api.authorize.net";
 	
 	
 	$path = "/xml/v1/request.api";
