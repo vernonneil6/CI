d820f41da6a5b669bd3ad0d810968cf45c809687
nathankairo <nathankairo@grossmaninteractive.com>
2015-03-13 09:35:16 -0400

    #20906

--- application/catalog/models/complaints.php
+++ application/catalog/models/complaints.php
@@ -2197,8 +2197,8 @@ class Complaints extends CI_Model
 	
 	function companystreetvalid($company, $street)
 	{
-		$query = $this->db->get_where('youg_company', array('company'=>$company, 'streetaddress'=>$street))->result_array();
-		$checkaselite=$query[0]['id'];
+		$query = $this->db->get_where('youg_company', array('company'=>$company, 'streetaddress'=>$street))->row_array();
+		$checkaselite=$query['id'];
 		$eliter=$this->db->get_where('youg_elite',array('company_id'=>$checkaselite, 'status'=>'Enable'));
 		if($eliter->num_rows() > 0)
 		{
@@ -2213,8 +2213,8 @@ class Complaints extends CI_Model
 	
 	function companystreetvalid1($company, $street)
 	{
-		$query = $this->db->get_where('youg_company', array('company'=>$company, 'companystreet'=>$street))->result_array();
-		$checkaselite=$query[0]['id'];
+		$query = $this->db->get_where('youg_company', array('company'=>$company, 'companystreet'=>$street))->row_array();
+		$checkaselite=$query['id'];
 		$eliter=$this->db->get_where('youg_elite',array('company_id'=>$checkaselite, 'status'=>'Enable'));
 		if($eliter->num_rows() > 0)
 		{
