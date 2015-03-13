85bf23e11814df04a2e33052a0de3b0ae3ef09f5
nathankairo <nathankairo@grossmaninteractive.com>
2015-03-13 17:13:35 -0400

    checkbox

--- application/catalog/controllers/solution.php
+++ application/catalog/controllers/solution.php
@@ -730,16 +730,16 @@ public function eliteSubscribe($formpost,$companyid) {
 	   $transactionkey="38UzuaL2c6y5BQ88";
 	   $host = "apitest.authorize.net"; */
 	
-	/*sandbox test mode*/
+	/*sandbox test mode
 	
 	  $loginname="83EK7S4R8qy3";
 	   $transactionkey="5Rx8Mn8PAS5s77gr";
-	   $host = "apitest.authorize.net";
+	   $host = "apitest.authorize.net";*/
 	
-	/*live
+	/*live*/
 	    $loginname="5h7G7Sbr";
 		$transactionkey="94KU7Sznk72Kj3HK";
-		$host = "api.authorize.net";*/
+		$host = "api.authorize.net";
 	
 	
 	$path = "/xml/v1/request.api";
