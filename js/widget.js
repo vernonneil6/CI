4019424ed99b771a3c1c931246fd5fc7c8db0cab
michaelkelton <michaelkelton@grossmaninteractive.com>
2015-05-06 15:07:20 +0100

    #21921 updates

--- js/widget.js
+++ js/widget.js
@@ -1,10 +1,11 @@
 function popupwindow(w, h) {
   var left = (screen.width/2)-(w/2);
   var top = (screen.height/2)-(h/2);
-  document.getElementById("review_popup").style.left = left+'px'; 
-  document.getElementById("review_popup").style.top = top+'px';
+  document.getElementById("review_popup").style.left = '50%'; 
+  document.getElementById("review_popup").style.top = '50%';
   document.getElementById("review_popup").style.width = w+'px';
   document.getElementById("review_popup").style.height = h+'px';  
+  document.getElementById("review_popup").style.transform = 'translateX(-50%) translateY(-50%)';  
 }
 
 function showPopup(){
