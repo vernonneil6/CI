c997971cf04f3641e63067097b90eab8d318f6aa
michaelkelton <michaelkelton@grossmaninteractive.com>
2015-06-09 16:55:09 +0100

    #22474 updates

--- application/catalog/views/header.php
+++ application/catalog/views/header.php
@@ -9,6 +9,9 @@
 </noscript>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title><?php echo $title;?></title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
 <meta name="keywords" content="<?php echo $keywords;?>">
 <meta name="description" content="<?php echo $description;?>">
 <script type="text/javascript" src="js/jquery-1.7.min.js"></script>
