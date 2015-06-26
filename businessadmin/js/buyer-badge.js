bc7159a1edf8fda66338a4b888131b70a675986b
Vernon Neil <vernonneil@grossmaninteractive.com>
2015-06-26 08:26:46 -0500

    ygr files

---
+++ businessadmin/js/buyer-badge.js
@@ -0,0 +1,45 @@
+
+//Disable right mouse click Script
+
+var message="This is an copyright seal";
+
+///////////////////////////////////
+function clickIE4(){
+if (event.button==2){
+alert(message);
+return false;
+}
+}
+
+function clickNS4(e){
+if (document.layers||document.getElementById&&!document.all){
+if (e.which==2||e.which==3){
+alert(message);
+return false;
+}
+}
+}
+
+if (document.layers){
+document.captureEvents(Event.MOUSEDOWN);
+
+document.getElementById("badge-seals").oncontextmenu=clickNS4;
+document.getElementById("buyer-seals").oncontextmenu=clickNS4;
+
+}
+else if (document.all&&!document.getElementById){
+document.getElementById("badge-seals").oncontextmenu=clickIE4;
+document.getElementById("buyer-seals").oncontextmenu=clickIE4;
+}
+
+document.getElementById("badge-seals").oncontextmenu=new Function("alert(message);return false");
+
+document.getElementById("buyer-seals").oncontextmenu=new Function("alert(message);return false");
+
+
+function disableRightClick(event) {
+	
+		alert('This is an copyright seal');
+        console.log("This is an copyright seal");
+		return false;
+}
