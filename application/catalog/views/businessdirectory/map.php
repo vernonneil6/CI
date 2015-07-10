122de59efecf2948709ca6dc0b16911685191e59
nathankairo <nathankairo@grossmaninteractive.com>
2015-07-10 09:27:27 -0400

    map direction

--- application/catalog/views/businessdirectory/map.php
+++ application/catalog/views/businessdirectory/map.php
@@ -1,69 +1,99 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
-
-<body>
-<div class="srch_result_blck">
-  <div class="innr_wrap">
-    <div class="srch_rslt_right">
-      <div class="map_wrap">
-        <div class="Flexible-container">
-          <?php /*?><iframe width="770" height="430" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBkUSG003UBp7IiqoZXZUJjtC_-N4BOZ_c
-    &q=<?php echo $address; ?>"></iframe><?php */?>
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCZKlrc_zm7xNNobdBpaFH8L-viQ-Wn3tE&sensor=false"></script>
-<script type="text/javascript">
-            // When the window has finished loading create our google map below
-            google.maps.event.addDomListener(window, 'load', init);
-        
-		<?php 
-	$json = file_get_contents("http://maps.google.com/maps/api/geocode/json?address=$address&sensor=false");
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <title>Travel modes in directions</title>
+    <style>
+      html, body, #map-canvas {
+        height: 100%;
+        margin: 0px;
+        padding: 0px
+      }
+      #panel {
+        position: absolute;
+        top: 5px;
+        left: 50%;
+        margin-left: -180px;
+        z-index: 5;
+        background-color: #fff;
+        padding: 5px;
+        border: 1px solid #999;
+      }
+    </style>
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true"></script>
+     <?php $json = file_get_contents("http://maps.google.com/maps/api/geocode/json?address=".$address."&sensor=false");
     $json = json_decode($json);
 
-    $lat = $json->{'results'}[0]->{'geometry'}->{'location'}->{'lat'};
-    $long = $json->{'results'}[0]->{'geometry'}->{'location'}->{'lng'};
-    
-		?>
-	      function init() {
-				
-				var myLatlng = new google.maps.LatLng(<?php echo $lat?>,<?php echo $long;?>);
-				
-                // Basic options for a simple Google Map
-                // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-                var mapOptions = {
-                    // How zoomed in you want the map to start at (always required)
-                    zoom: 14,
+     $lat = $json->{'results'}[0]->{'geometry'}->{'location'}->{'lat'};
+     $long = $json->{'results'}[0]->{'geometry'}->{'location'}->{'lng'}; 
+     // current Ip get lat long
+     //$ip = '107.170.167.130';
+     $ip = $_SERVER['REMOTE_ADDR'];
+     $d  = file_get_contents("http://www.geoplugin.net/php.gp?ip=$ip");
 
-                    // The latitude and longitude to center the map (always required)
-                    center: new google.maps.LatLng(<?php echo $lat?>,<?php echo $long;?>), // New York
+		$data = unserialize($d);
+		$clatitude = $data['geoplugin_latitude'];
+		$clongitude = $data['geoplugin_longitude'];
+     
+     ?>
+    <script>
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
 
-                    // How you would like to style the map. 
-                    // This is where you would paste any style found on Snazzy Maps.
-                    //styles: [	{		featureType:'water',		stylers:[{color:'#0080FF'},{visibility:'on'}]	},{		featureType:'landscape',		stylers:[{color:'#f2f2f2'}]	},{		featureType:'road',		stylers:[{saturation:-100},{lightness:45}]	},{		featureType:'road.highway',		stylers:[{visibility:'simplified'}]	},{		featureType:'road.arterial',		elementType:'labels.icon',		stylers:[{visibility:'off'}]	},{		featureType:'administrative',		elementType:'labels.text.fill',		stylers:[{color:'#444444'}]	},{		featureType:'transit',		stylers:[{visibility:'off'}]	},{		featureType:'poi',		stylers:[{visibility:'off'}]	}]
-                    //styles : [ {   featureType: "all",   stylers: [     { saturation: -80 }    ]  },{    featureType: "road.arterial",    elementType: "geometry",    stylers: [      { hue: "#00ffee" },      { saturation: 100 }    ]  },{    featureType: "poi.business",    elementType: "labels",    stylers: [      { visibility: "off" }    ]  }]                
-                };
+var from = new google.maps.LatLng(<?php echo $clatitude;?> , <?php echo $clongitude;?> );
+var to = new google.maps.LatLng(<?php echo $lat;?> , <?php echo $long;?> );
 
-                // Get the HTML DOM element that will contain your map 
-                // We are using a div with id="map" seen below in the <body>
-                var mapElement = document.getElementById('map');
+// Test Lat long
+//var haight = new google.maps.LatLng(28.1287597, -82.5076474);
+//var oceanBeach = new google.maps.LatLng(28.1279088, -82.5046026);
 
-                // Create the Google Map using out element and options defined above
-                var map = new google.maps.Map(mapElement, mapOptions);
-                
-                var marker = new google.maps.Marker({
-					  position: myLatlng,
-					  map: map
-				});
-            }
-        </script>
-<div id="map" style="width:800px;height:650px;"></div>
+function initialize() {
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  var mapOptions = {
+    zoom: 14,
+    center: from
+  }
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  directionsDisplay.setMap(map);
+}
 
-        </div>
-      </div>
+function calcRoute() {
+  var selectedMode = document.getElementById('mode').value;
+  var request = {
+      origin: from,
+      destination: to,
+      // Note that Javascript allows us to access the constant
+      // using square brackets and a string value as its
+      // "property."
+      travelMode: google.maps.TravelMode[selectedMode]
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    }
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+    </script>
+  </head>
+  <body>
+	  
+    <div id="panel">
+		<?php //echo $clatitude.',' .$clongitude;?>
+		<?php //echo $lat.','.$long;?>
+    <b>Mode of Travel: </b>
+    <select id="mode" onchange="calcRoute();">
+      <option value="DRIVING">Driving</option>
+      <option value="WALKING">Walking</option>
+      <option value="BICYCLING">Bicycling</option>
+      <option value="TRANSIT">Transit</option>
+    </select>
     </div>
-  </div>
-</div>
-</body>
+   
+    <div id="map-canvas"></div>
+  </body>
 </html>
