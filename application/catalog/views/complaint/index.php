f91b3eabce2da09f906893fe072c453c24a52fd5
vickkeane <vickkeane@outlook.com>
2015-07-31 09:57:50 +0000

    complaint page design issue fixed

--- application/catalog/views/complaint/index.php
+++ application/catalog/views/complaint/index.php
@@ -11,19 +11,31 @@
      <!-- Go to www.addthis.com/dashboard to customize your tools -->
 				<div class="addthis_native_toolbox"></div>
       <div class="dir_rew_wrap">
-        <?php if(count($complaints)>0){ ?>
-        <?php for($i=0; $i<count($complaints); $i++) {
-		$elitemem_status = $this->common->get_eliteship_bycompanyid($complaints[$i]['companyid']);	
+        <?php if(count($complaints)>0)
+        { 
+	     for($i=0; $i<count($complaints); $i++) 
+         {
+		    $elitemem_status = $this->common->get_eliteship_bycompanyid($complaints[$i]['companyid']);	
 			 ?>
-        <?php $user = $this->common->get_user_byid($complaints[$i]['userid']);?>
-        <div class="revw_blck">
+          <?php $user = $this->common->get_user_byid($complaints[$i]['userid']);?>
+          <div class="revw_blck">
           <?php /*?><div class="revw_blck_img"> <a href="<?php echo site_url('complaint/browse/'.$complaints[$i]['comseokeyword']); ?>" title="view complaint detail"><img src="<?php if( $complaints[$i]['logo'] ){ echo $this->common->get_setting_value('2').$this->config->item('company_thumb_upload_path');?><?php echo stripslashes($complaints[$i]['logo']); } else{echo $this->common->get_setting_value('2').$this->config->item('company_thumb_upload_path')."/no_image.png"; } ?>" alt="<?php echo ucfirst(stripslashes($complaints[$i]['company'])); ?>" width="100px" height="40px"/></a> </div><?php */?>
           <?php if(count($elitemem_status)==0){?>
-        <div class="revw_blck_img"><div class="vry_logo"> <a href="<?php echo site_url($complaints[$i]['company_seoslug']);?>" title="view company Detail"><img src="images/notverified.png" class = "reviewnotverifiedlogo" alt="<?php echo ucfirst(stripslashes($complaints[$i]['company'])); ?>" /></a> </div> </div>
-        <?php }else{
+          <div class="revw_blck_img">
+			  <div class="vry_logo"> 
+				  <a href="<?php echo site_url($complaints[$i]['company_seoslug']);?>" title="view company Detail"><img src="images/notverified.png" class = "reviewnotverifiedlogo" alt="<?php echo ucfirst(stripslashes($complaints[$i]['company'])); ?>" />
+				  </a> 
+				  </div> 
+				  </div>
+          <?php }else{
 				  ?>
-        <div class="revw_blck_img"><div class="vry_logo"> <a href="<?php echo site_url($complaints[$i]['company_seoslug']);?>" title="view company Detail"><img src="images/verifiedlogo.jpg" class = "reviewverifiedlogo" alt="<?php echo ucfirst(stripslashes($complaints[$i]['company'])); ?>" /></a> </div></div>
-        <?php
+          <div class="revw_blck_img">
+			  <div class="vry_logo"> 
+				  <a href="<?php echo site_url($complaints[$i]['company_seoslug']);?>" title="view company Detail"><img src="images/verifiedlogo.jpg" class = "reviewverifiedlogo" alt="<?php echo ucfirst(stripslashes($complaints[$i]['company'])); ?>" />
+				  </a> 
+			  </div>
+		  </div>
+          <?php
 				  } ?>
           <?php 
                         //$date = date_default_timezone_set('Asia/Kolkata');                             
@@ -35,7 +47,7 @@
                         $newdate =round(($d1-$d2)/60);
                         if($newdate > 60){$diff = round(($d1-$d2)/60/60).' hours ago';}else{$diff = $newdate.' minutes ago';}
                         ?>
-          <div class="revw_blck_cnt">
+           <div class="revw_blck_cnt">
             <h2> <a href="<?php echo site_url($complaints[$i]['seoslug']); ?>" title="view complaint detail" class="complaintlist"> <?php echo strtoupper($complaints[$i]['company']);?> </a> </h2>
             <div class="revw_occupt"> <span></span>-
               <p>
@@ -52,15 +64,18 @@
             <div class="revw_desc"> " <a href="<?php echo site_url($complaints[$i]['seoslug']); ?>" title="view complaint detail"><?php echo strtolower(substr(stripslashes($complaints[$i]['detail']),0,212)."..."); ?></a>" </div>
             <div class="revw_ratpoint"> <span> <a href="<?php echo site_url($complaints[$i]['seoslug']); ?>" title="view complaint detail">Reported Damage: $<?php echo $complaints[$i]['damagesinamt'];?> </a> </span>
             </div>
-          </div>
-          <?php } ?>
+           </div>
+           </div>
+			<?php
+		 } 
+        }
+        ?>
         </div>
         <?php 
 		if($this->pagination->create_links()) { ?>
         <div class="pagination"><?php echo $this->pagination->create_links(); ?></div>
         <?php } 
 	   ?>
-        <?php } ?>
       </div>
     </div>
   </section>
