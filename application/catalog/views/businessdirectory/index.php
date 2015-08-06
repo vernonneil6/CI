388514ba71845a2ff355fbddd4acd1caba1a3440
Vernon Neil <vernonneil@grossmaninteractive.com>
2015-08-06 20:13:22 +0100

    business search function

--- application/catalog/views/businessdirectory/index.php
+++ application/catalog/views/businessdirectory/index.php
@@ -10,16 +10,23 @@
 		</div>
 		
 		<div class='headersearch'>
-		  <?php echo form_open('businessdirectory/search',array('class'=>'formBox','name'=>'frmsearch','id'=>'frmcompany')); ?>
-			  <?php if( $this->uri->segment(1)=='complaint' && $this->uri->segment(2)=='search') { $serkeyword=base64_decode($this->uri->segment(3));} else { $serkeyword =''; } ?>
-			<input type='text'  class='headersearchbar' placeholder="Search for a Business..." name="searchcomp"  id="search" value="<?php echo $serkeyword;?>" required maxlength="30">
-			<button type="submit" class="headersearchbtn fa fa-search" value="" name="btnsearch"></button>
-			<div class="orwrp">
-				<div class="orinnrwrp"> </div>
-				<h1 class="bus_tag"><a href="<?php echo site_url('businessdirectory/add');?>" title="Submit Business To YouGotRated Directory" class="complaintlist">SUBMIT A NEW BUSINESS</a></h1>
-			</div>
-			<div class="lgn_btnlogo"> <a href="<?php echo base_url();?>"><img src="images/ygr_logos.png" class="logo_btm" alt="Yougotrated" title="Yougotrated"></a> </div>
-		 <?php echo form_close();?> 
+			
+		<form method="GET" action="searchresult" id="business_search_form" class="formBox">
+			<input type="text" class="headersearchbar" name="query" placeholder="Search for a Business..."  id="suggest"
+				autocomplete="off" value="<?php isset($_GET['query'])?htmlentities($_GET['query']):''?>">
+
+			<input type="hidden" class="input-large" name="city" id="city"  value="<?=isset($_GET['city'])?htmlentities($_GET['city']):''?>"> 
+			<input type="hidden" class="input-large" name="state" id="state"  value="<?=isset($_GET['state'])?htmlentities($_GET['state']):''?>"> 
+							
+			<button id="send" type="submit" class="headersearchbtn fa fa-search" value="Submit" name="btnsearch"></button>
+			
+		<div class="orwrp">
+		<div class="orinnrwrp"> </div>
+		<h1 class="bus_tag"><a href="<?php echo site_url('businessdirectory/add');?>" title="Submit Business To YouGotRated Directory" class="complaintlist">SUBMIT A NEW BUSINESS</a></h1>
+		</div>
+		<div class="lgn_btnlogo"> <a href="<?php echo base_url();?>"><img src="images/ygr_logos.png" class="logo_btm" alt="Yougotrated" title="Yougotrated"></a> </div>
+
+		</form>					 
 		</div>
     </div>
   </section>
