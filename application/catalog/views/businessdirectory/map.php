c1f57d24efd35d52a95dd57eb0e567a4ba3369e7
vickkeane <vickkeane@outlook.com>
2015-08-18 10:21:42 +0000

    Map in iphone #23378

--- application/catalog/views/businessdirectory/map.php
+++ application/catalog/views/businessdirectory/map.php
@@ -5,31 +5,25 @@
     <meta charset="utf-8">
     <title>Directions service</title>
     <style>
-      html, body, #map-canvas {
-        height: 100%;
-        margin: 0px;
-        padding: 0px
-      }
+		@media only screen and (min-width: 310px) and (max-width: 770px)  
+		{
+			#panel {position: absolute;left: 0;margin-left: 0px;z-index: 5;background-color: #fff;padding: 5px;float: left;clear: left;top: 8%;}
+		}
+        html, body, #map-canvas 
+        {
+          height: 100%;
+          margin: 0px;
+          padding: 0px
+        }
       #error{color:red;display:none;text-align:center; }
-		  
-		  
-	 
-      #panel {
-        position: absolute;
-        top: 5px;
-        left: 24%;
-        margin-left: -180px;
-        z-index: 5;
-        background-color: #fff;
-        padding: 5px;
-        border: 1px solid #999;
-      }
+	  .search { background:#005cb9;color:#fff;border-style:none;padding: 5px;} 
+      
     </style>
     <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     <?php 
-    //$ip = '107.170.167.130';
-     $ip = $_SERVER['REMOTE_ADDR'];
+    $ip = '107.170.167.130';
+     //$ip = $_SERVER['REMOTE_ADDR'];
      $d  = file_get_contents("http://www.geoplugin.net/php.gp?ip=$ip");
 
 		$data = unserialize($d);
@@ -40,6 +34,8 @@
 var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
 var map;
+var initialLocation;
+var browserSupportFlag =  new Boolean();
 
 function initialize() {
 	
@@ -52,12 +48,34 @@ function initialize() {
   };
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   directionsDisplay.setMap(map);
+  var marker = new google.maps.Marker({
+    position: chicago
+  
+});
+
+// To add the marker to the map, call setMap();
+marker.setMap(map);
+	navigator.geolocation.getCurrentPosition(function(position) {
+            var geocoder = new google.maps.Geocoder();
+            geocoder.geocode({
+              //"location": new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+              "location": new google.maps.LatLng(<?php echo $clatitude;?> , <?php echo $clongitude;?> )
+            },
+            function(results, status) {
+              if (status == google.maps.GeocoderStatus.OK)
+                $("#start").val(results[0].formatted_address);
+              else
+                $("#error").append("Unable to retrieve your address<br />");
+            });
+             });
+
 }
 
-function calcRoute(start,end,mode) {
+function calcRoute(start,end) {
+
   //var start = document.getElementById('start').value;
   //var end = document.getElementById('end').value;
-  var selectedMode =mode;
+  var selectedMode ='DRIVING';
   var request = {
       origin:start,
       destination:end,
@@ -65,6 +83,7 @@ function calcRoute(start,end,mode) {
   };
   directionsService.route(request, function(response, status) {
     if (status == google.maps.DirectionsStatus.OK) {
+		
 		 $("#error").hide();
       directionsDisplay.setDirections(response);
     }
@@ -78,10 +97,12 @@ function calcRoute(start,end,mode) {
 
 google.maps.event.addDomListener(window, 'load', initialize);
   $(document).ready(function() {
+	  
+	   
 	   $("#calculate-route").submit(function(event) {
           event.preventDefault();
         
-          calcRoute($("#start").val(), $("#end").val(),$("#mode").val());
+          calcRoute($("#start").val(), $("#end").val());
         });
       });
     </script>
@@ -89,20 +110,13 @@ google.maps.event.addDomListener(window, 'load', initialize);
   <body>
     <div id="panel">
 		<form id="calculate-route" name="calculate-route" action="#" method="get">
-    <b>Start: </b>
-   <input type="text" id="start">
-    <b>End: </b>
+   
+   <input type="hidden" id="start">
+   
     <input type="text" id="end">
-    <b> Mode:</b>
-     <select id="mode">
-	  <option value="DRIVING">Select Your Travel Mode</option>
-      <option value="DRIVING">Driving</option>
-      <option value="WALKING">Walking</option>
-      <option value="BICYCLING">Bicycling</option>
-      <option value="TRANSIT">Transit</option>
-    </select>
-    <input type="submit" />
-      <input type="reset" />
+    
+    <input type="submit" value="Get Direction" class="search" />
+     
        <p id="error"><b>Unable to retrieve your route</b><br /></p>
     </form>
     </div>
