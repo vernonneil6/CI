b8aeb0784268b5f4bff2bb70e8c66d9e2b54e219
vickkeane <vickkeane@outlook.com>
2015-08-26 17:10:49 +0000

    #23487 Embedable widget - must load only when clicked

--- application/catalog/views/widget/widget.php
+++ application/catalog/views/widget/widget.php
@@ -1,10 +1,10 @@
 <link rel="stylesheet" href="<?php echo 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME'].'/css/widget.css'; ?>" type="text/css">
 <script type="text/javascript" src="<?php echo 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME'].'/js/widget.js'; ?>" ></script>
 
-<div class = "company_review_tab fancybox" href="#review_popup"  onclick="showPopup()"></div>
+<div class = "company_review_tab fancybox" href="#review_popup" onclick="showPopup('<?php echo 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME']. '/widget/content/'.$companyid; ?>')"></div>
 <div class="review_cover" id="review_cover">	
 <div id="review_popup" class = "popupwidth">
 	<div class='close_popup' onclick="closePopup()"> </div>		
-	<iframe id="container_frame" src="<?php echo 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME']. '/widget/content/'.$companyid; ?>"  class="widget_popup"></iframe>
+	
 </div> 
 </div>
--- js/widget.js
+++ js/widget.js
@@ -1,4 +1,5 @@
-function popupwindow(w, h) {
+function popupwindow(w, h,src) {
+	
   var left = (screen.width/2)-(w/2);
   var top = (screen.height/2)-(h/2);
   document.getElementById("review_popup").style.left = '50%'; 
@@ -6,13 +7,24 @@ function popupwindow(w, h) {
   document.getElementById("review_popup").style.width = w+'px';
   document.getElementById("review_popup").style.height = h+'px';  
   document.getElementById("review_popup").style.transform = 'translateX(-50%) translateY(-50%)';  
+	//var iframe = document.getElementById("container_frame");
+//var iframe_contents = iframe.contentDocument.body.innerHTML;
+//alert(iframe_contents);
+var target = document.getElementById("review_popup");
+ifrm = document.createElement("IFRAME");
+ifrm.setAttribute("src", src);
+ifrm.style.width =  w+'px';
+ifrm.style.height = h+'px';  
+target.appendChild(ifrm);
+  
 }
 
-function showPopup(){
- popupwindow('500','565');
+function showPopup(src){
+ popupwindow('500','565',src);
  document.body.style.overflow = "hidden";
  document.getElementById('review_cover').style.display = 'block';
  document.getElementById('review_cover').style.visibility = 'visible';
+  
 }
 
 function closePopup(){
