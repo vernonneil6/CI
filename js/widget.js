a2ab5d5f03bd3a929c3483386ce3f4d2934c5c09
vickkeane <vickkeane@outlook.com>
2015-08-27 14:59:28 +0000

    #23500 Viewing widget on mobile devices

--- js/widget.js
+++ js/widget.js
@@ -1,21 +1,23 @@
 function popupwindow(w, h,src) {
-	
-  var left = (screen.width/2)-(w/2);
-  var top = (screen.height/2)-(h/2);
-  document.getElementById("review_popup").style.left = '50%'; 
-  document.getElementById("review_popup").style.top = '50%';
-  document.getElementById("review_popup").style.width = w+'px';
-  document.getElementById("review_popup").style.height = h+'px';  
-  document.getElementById("review_popup").style.transform = 'translateX(-50%) translateY(-50%)';  
-	//var iframe = document.getElementById("container_frame");
+	  //var swidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+	 //var width=(screen.width/100)*80;
+	//if(swidth<600){ var width=(screen.width/100)*80}else{ var width=w;}
+       //var left = (screen.width/2)-(w/2);
+      //var top = (screen.height/2)-(h/2);
+     //document.getElementById("review_popup").style.left = '50%'; 
+    //document.getElementById("review_popup").style.top = '50%';
+   //document.getElementById("review_popup").style.width = w+'px';
+  //document.getElementById("review_popup").style.height = h+'px';  
+ //document.getElementById("review_popup").style.transform = 'translateX(-50%) translateY(-50%)';  
+//var iframe = document.getElementById("container_frame");
 //var iframe_contents = iframe.contentDocument.body.innerHTML;
 //alert(iframe_contents);
+
+
 var target = document.getElementById("review_popup");
 ifrm = document.createElement("IFRAME");
 ifrm.setAttribute("src", src);
-ifrm.style.width =  w+'px';
-ifrm.style.height = h+'px';  
-target.appendChild(ifrm);
+ target.appendChild(ifrm);
   
 }
 
