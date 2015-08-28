426d51781498f8a9a8a9548d95c7e81c10be3fc7
nathankairo <nathankairo@grossmaninteractive.com>
2015-08-28 05:53:24 -0400

    iframe responsive

--- js/widget.js
+++ js/widget.js
@@ -1,22 +1,9 @@
 function ygrembeddedwidgetpopupwindow(w, h,src) {
-	  //var swidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-	 //var width=(screen.width/100)*80;
-	//if(swidth<600){ var width=(screen.width/100)*80}else{ var width=w;}
-       //var left = (screen.width/2)-(w/2);
-      //var top = (screen.height/2)-(h/2);
-     //document.getElementById("review_popup").style.left = '50%'; 
-    //document.getElementById("review_popup").style.top = '50%';
-   //document.getElementById("review_popup").style.width = w+'px';
-  //document.getElementById("review_popup").style.height = h+'px';  
- //document.getElementById("review_popup").style.transform = 'translateX(-50%) translateY(-50%)';  
-//var iframe = document.getElementById("container_frame");
-//var iframe_contents = iframe.contentDocument.body.innerHTML;
-//alert(iframe_contents);
-
-
+	  
 var target = document.getElementById("review_popup");
 ifrm = document.createElement("IFRAME");
 ifrm.setAttribute("src", src);
+ifrm.setAttribute("id", 'ygrembbediframe');
  target.appendChild(ifrm);
   
 }
@@ -26,17 +13,18 @@ function ygrembeddedwidgetshowPopup(src){
  document.body.style.overflow = "hidden";
  document.getElementById('review_cover').style.display = 'block';
  document.getElementById('review_cover').style.visibility = 'visible';
+
   
 }
 
 function ygrembeddedwidgetclosePopup(){
- document.getElementById('review_cover').style.display = 'none'; 
+ document.getElementById('review_cover').style.display = 'none';
+ var element = document.getElementById("ygrembbediframe");
+element.parentNode.removeChild(element); 
+
  document.body.style.overflow = "visible";
  document.getElementById('review_cover').style.visibility = 'hidden';
- var iframes = document.getElementsByTagName('iframe');
-for (var i = 0; i < iframes.length; i++) {
-    iframes[i].parentNode.removeChild(iframes[i]);
-}
+
 }
 
 
